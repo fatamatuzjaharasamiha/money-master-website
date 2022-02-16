50abcc4ddfcd8da9624997d7997b935fa22de90d
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-02-17 03:29:02 +0600

    javascript dom

--- script/app.js
+++ script/app.js
@@ -1,3 +1,13 @@
 document.getElementById('Calculate-btn').addEventListener('click', function () {
-    console.log('clicked');
+    const incomeAmount = document.getElementById('income-amount')
+    let incomeValue = incomeAmount.value;
+    // console.log(incomeValue);
+    const foodCost = document.getElementById('food-cost');
+    const foodValue = foodCost.value;
+    // console.log(foodValue);
+    const rentCost = document.getElementById('rent-cost');
+    const rentValue = rentCost.value;
+    const rentCost = document.getElementById('rent-cost');
+    const rentValue = rentCost.value;
+    
 })
