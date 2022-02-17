56a8abf316daa2a848338ceab4c17f19f8a0f4dc
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-02-17 07:27:21 +0600

    income part

--- script/app.js
+++ script/app.js
@@ -1,13 +1,32 @@
+
 document.getElementById('Calculate-btn').addEventListener('click', function () {
+
     const incomeAmount = document.getElementById('income-amount')
-    let incomeValue = incomeAmount.value;
-    // console.log(incomeValue);
+    const incomeValue = parseFloat(incomeAmount.value);
+
+
     const foodCost = document.getElementById('food-cost');
-    const foodValue = foodCost.value;
-    // console.log(foodValue);
+    const foodValue = parseFloat(foodCost.value);
+
     const rentCost = document.getElementById('rent-cost');
-    const rentValue = rentCost.value;
-    const rentCost = document.getElementById('rent-cost');
-    const rentValue = rentCost.value;
-    
+    const rentValue = parseFloat(rentCost.value);
+
+    const clothCost = document.getElementById('cloth-cost');
+    const clothValue = parseFloat(clothCost.value);
+    //total expanses calculation
+    let totalExpanse = foodValue + rentValue + clothValue;
+    const total = document.getElementById('total')
+    total.innerText = totalExpanse;
+
+    //Balance Calculation
+    //     let Balance = incomeValue - totalExpanse;
+    const updateBalance = document.getElementById('update-balance');
+    updateBalance.innerText = Balance;
+
+
 })
+//Saving part
+document.getElementById('save-button').addEventListener('click', function () {
+
+})
+
