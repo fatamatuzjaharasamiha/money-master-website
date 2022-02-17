949caf1cc40213a1666933d7f5ed6d5e6d5ffb72
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-02-17 15:22:16 +0600

    responsive done

--- script/app.js
+++ script/app.js
@@ -1,29 +1,44 @@
-function getInputValue(boxId){
-    let inputBox = document.getElementById(boxId);
-    let inputAmount = parseFloat
-}
-
+//get value from input box
 
 document.getElementById('Calculate-btn').addEventListener('click', function () {
 
     const incomeAmount = document.getElementById('income-amount')
     const incomeValue = parseFloat(incomeAmount.value);
+
     const foodCost = document.getElementById('food-cost');
     const foodValue = parseFloat(foodCost.value);
+
     const rentCost = document.getElementById('rent-cost');
     const rentValue = parseFloat(rentCost.value);
+
+
     const clothCost = document.getElementById('cloth-cost');
     const clothValue = parseFloat(clothCost.value);
+
+    if (isNaN(incomeValue) || incomeValue < 0) {
+        alert('Please input valid amount')
+    }
+    if (isNaN(foodValue) || foodValue < 0) {
+        alert('Please input valid cost amount')
+    }
+    if (isNaN(rentValue) || rentValue < 0) {
+        alert('Please input valid cost amount')
+    }
+    if (isNaN(clothValue) || clothValue < 0) {
+        alert('Please input valid cost amount')
+    }
+
     //total expanses calculation
-    let totalExpanse = foodValue + rentValue + clothValue;
+
     const total = document.getElementById('total')
+    let totalExpanse = foodValue + rentValue + clothValue;
     total.innerText = totalExpanse;
 
     //Balance Calculation
-    let Balance = incomeValue - totalExpanse;
-    const updateBalance = document.getElementById('update-balance');
-    updateBalance.innerText = Balance;
 
+    const updateBalance = document.getElementById('update-balance');
+    let balance = incomeValue - totalExpanse;
+    updateBalance.innerText = balance;
 
 })
 //Saving part
@@ -36,20 +51,17 @@ document.getElementById('save-button').addEventListener('click', function () {
     const incomeAmount = document.getElementById('income-amount')
     const incomeValue = parseFloat(incomeAmount.value);
 
-    const saving = (incomeValue * saveValue) / 100;
+
+
     const savingAmount = document.getElementById('saving-amount');
+    const saving = (incomeValue * saveValue) / 100;
     savingAmount.innerText = saving;
 
+
     const updateBalance = document.getElementById('update-balance');
-    const balance = updateBalance.innerText
-    let remainingBalance = balance - saving
+    const balance = updateBalance.innerText;
     let remainingText = document.getElementById('remaining');
+    let remainingBalance = balance - saving;
     remainingText.innerText = remainingBalance;
-
-
-
-
-
-
 })
 
