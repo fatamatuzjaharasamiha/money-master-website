d57836fd6f2ab39c1e9b23bce925a55c79539f08
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-02-17 10:52:25 +0600

    savings part

--- script/app.js
+++ script/app.js
@@ -1,16 +1,17 @@
+function getInputValue(boxId){
+    let inputBox = document.getElementById(boxId);
+    let inputAmount = parseFloat
+}
+
 
 document.getElementById('Calculate-btn').addEventListener('click', function () {
 
     const incomeAmount = document.getElementById('income-amount')
     const incomeValue = parseFloat(incomeAmount.value);
-
-
     const foodCost = document.getElementById('food-cost');
     const foodValue = parseFloat(foodCost.value);
-
     const rentCost = document.getElementById('rent-cost');
     const rentValue = parseFloat(rentCost.value);
-
     const clothCost = document.getElementById('cloth-cost');
     const clothValue = parseFloat(clothCost.value);
     //total expanses calculation
@@ -19,14 +20,36 @@ document.getElementById('Calculate-btn').addEventListener('click', function () {
     total.innerText = totalExpanse;
 
     //Balance Calculation
-    //     let Balance = incomeValue - totalExpanse;
+    let Balance = incomeValue - totalExpanse;
     const updateBalance = document.getElementById('update-balance');
     updateBalance.innerText = Balance;
 
 
 })
 //Saving part
+
 document.getElementById('save-button').addEventListener('click', function () {
+
+    const saveInput = document.getElementById('save-input');
+    const saveValue = parseFloat(saveInput.value);
+
+    const incomeAmount = document.getElementById('income-amount')
+    const incomeValue = parseFloat(incomeAmount.value);
+
+    const saving = (incomeValue * saveValue) / 100;
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = saving;
+
+    const updateBalance = document.getElementById('update-balance');
+    const balance = updateBalance.innerText
+    let remainingBalance = balance - saving
+    let remainingText = document.getElementById('remaining');
+    remainingText.innerText = remainingBalance;
+
+
+
+
+
 
 })
 
